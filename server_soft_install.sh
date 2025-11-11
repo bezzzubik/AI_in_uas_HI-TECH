6e1f21d3c5bde22f872c7f40d24f2563c8db54be
BezZzuBiK <46130330+bezzzubik@users.noreply.github.com>
2025-11-11 12:34:48 +0500

    Дополнение python библиотек

--- server_soft_install.sh
+++ server_soft_install.sh
@@ -35,7 +35,7 @@ sudo apt install ros-noetic-cv-bridge ros-noetic-image-transport ros-noetic-visi
 # Python‑библиотеки для работы с видео
 
 sudo apt install python3-pip -y
-pip3 install flask opencv-python opencv-contrib-python numpy
+pip3 install flask>=2.3.0 opencv-python>=4.8.0 opencv-contrib-python ultralytics>=8.0.0 numpy>=1.24.0 torch>=2.0.0 torchvision>=0.15.0
 
 
 # Проверка списка пакетов
